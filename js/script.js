6ddf3cc0bc906dc16010c9ec3f2080889f286507
zaa <zaa@tk-barn.ru>
2019-05-22 16:21:41 +0700

    Первое готово

--- js/script.js
+++ js/script.js
@@ -1,145 +1,51 @@
+var licenses;
 $(document).ready(function () {
-    //создаем поле игры
-    drawPole();
-    var colorDuble = ['', ''];
-    var mixArr = [];
-    var timer_id = null;
-    var indexOneTmp;
-    var indexTwoTmp;
-//обработчик кнопки старт
-    $('#btnStart').click(function () {
-        //при повторном нажатии проверяем есть ли активный таймер, если да то останавливаем
-        if (timer_id !== null) {
-            clearInterval(timer_id);
-            timer_id = null;
-        }
-        var seconds = 0;
-        //вешаем таймер
-        timer_id = setInterval(function () {
-            seconds++;
-            var time = moment.utc(seconds * 1000).format('HH:mm:ss');
-            $('#timer').text(time);
-        }, 1000);
-        //очищаем клетки
-        $('div .box').each(function (index, block) {
-            block.style.backgroundColor = 'rgb(255,255,255)';
-        });
-        //генерируем цвета для боксов
-        mixArr = getColorsArr();
-        colorDuble[0] = '';
-        colorDuble[1] = '';
-    });
-    $('#btnStop').click(function () {
-        if (timer_id !== null) {
-            clearInterval(timer_id);
-            timer_id = null;
-            mixArr = [];
-            $('div .box').each(function (index, block) {
-                block.style.backgroundColor = 'rgb(255,255,255)';
-            });
-            $('#timer').text('00:00:00');
-            alert('Игра окончена');
+    $.ajax({
+        url: "lic.json",
+        type: "POST",
+        crossDomain: true,
+        dataType: "json",
+        success: function (response) {
+            licenses = response;
+            $('#licplan1').html(response[0].licsum);
+            $('#licplan2').html(response[1].licsum);
+            $('#licplan3').html(response[2].licsum);
+            //заполняем список количеством лицензий первого типа
+            selAdd(parseInt(response[0].liccount));
+            let checked = $("input:radio:checked")[0].value;
+            let selectcol = $('#numlic').val();
+            let sumtot = sumLic(selectcol, licenses[parseInt(checked)].licsum);
+            //присваиваем начальный тотал
+            $('#total').html('$' + sumtot);
         }
     });
-//обработчик нажатий на боксы
-    $('div .box').click(function () {
-        if (mixArr.length != 16) {
-            alert('Начните игру нажав старт');
-            return false;
-        }
-        var boxIndex = $('div .box').index(this);
-        //делаем проверку, чтобы не обрабатывать повторно закрашенные цвета
-        if ($('div .box').eq(boxIndex)[0].style.backgroundColor == 'rgb(255, 255, 255)') {
-            if (colorDuble[0] === '') {
-                //если первое значение массива сравнения пары пустое, просто красим
-                colorDuble[0] = mixArr[boxIndex];
-                indexOneTmp = boxIndex;
-                $(this)[0].style.backgroundColor = mixArr[boxIndex];
-            } else if (colorDuble[0] !== '') {
-                //если первая клетка из пары уже выбрана, красим вторую и сравниваем
-                colorDuble[1] = mixArr[boxIndex];
-                indexTwoTmp = boxIndex;
-                $(this)[0].style.backgroundColor = mixArr[boxIndex];
-                //проверяем одинаковые или нет, если нет очищаем массив и красим в белый цвет
-                if (colorDuble[0] !== colorDuble[1]) {
-                    colorDuble[0] = '';
-                    colorDuble[1] = '';
-                    setTimeout(function () {
-                        $('div .box').eq(indexOneTmp)[0].style.backgroundColor = 'rgb(255, 255, 255)';
-                        $('div .box').eq(indexTwoTmp)[0].style.backgroundColor = 'rgb(255, 255, 255)';
-                    }, 100);
-                } else if (colorDuble[0] === colorDuble[1]) {
-                    colorDuble[0] = '';
-                    colorDuble[1] = '';
-                    indexOneTmp = null;
-                    indexTwoTmp = null;
-                    //проверяем закончилась ли игра
-                    var end = getTimeEnd();
-                    if (end == 1) {
-                        let timeend = $('#timer').text();
-                        clearInterval(timer_id);
-                        timer_id = null;
-                        alert('Вы выиграли!\n Затраченное время: ' + timeend);
-                    }
-                }
+    //меняем тотал при смене количества
+    $("#numlic").change(function () {
+        let checked = $("input:radio:checked")[0].value;
+        let selectcol = $(this).val();
+        let sumtot = sumLic(selectcol, licenses[parseInt(checked)].licsum);
+        $('#total').html('$' + sumtot);
+    });
 
-            }
-
-        }
+    $('input:radio[name=radiogroup]').on('change', function () {
+        //получаем количество лицензий для текущего плана
+        let countlic = parseInt(licenses[$(this).val()].liccount);
+        $('#numlic').empty();
+        selAdd(countlic);
+        let total = sumLic(1, parseInt(licenses[$(this).val()].licsum));
+        $('#total').html('$' + total);
+        $('#licplan').html('#' + (parseInt($(this).val())+1));
     });
+
 });
-//функция возвращает массив цветов для квадратов поля
-function getColorsArr() {
-    //массив уникальных цветов
-    var colorsArr = new Array();
-    var sortArr = new Array();
-    //формируем массив из 8 уник цветов для пар
-    for (let j = 0; j <= 7; j++) {
-        var color = genСolor();
-        if ($.inArray(color, colorsArr) == -1) {
-            colorsArr[j] = color;
-        }
-    }
-    //массив всех цветов для блоков
-    var colorAllArr = new Array();
-    var pos = 0;
-    $.each(colorsArr, function (index, value) {
-        colorAllArr[pos] = value;
-        colorAllArr[pos + 1] = value;
-        pos = pos + 2;
-    });
-    //мешаем цвета в массиве
-    sortArr = colorAllArr.sort(function () {
-        return Math.random() - 0.5;
-    });
-    return sortArr;
-}
-//функция создания поля игры
-function drawPole() {
-    //создаем поле квадратов игры при загрузке
-    for (let i = 0; i < 4; i++) {
-        $('#borderPoleBox').append('<div class="rowbox" id="rowbox' + i + '"></div>');
-        for (let j = 0; j < 4; j++) {
-            $('#rowbox' + i).append('<div class="box"' + ' ' + 'id="box' + i + j + '"></div>');
-        }
+
+function selAdd(counts) {
+    for (let i = 0; i < counts; i++) {
+        $('#numlic').append($('<option value=' + (i + 1) + '>' + (i + 1) + '</option>'));
     }
 }
-//функция генерации цвета RGB
-function genСolor() {
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
-    var random_color = 'rgb(' + r + ',' + g + ',' + b + ')';
-    return random_color;
-}
-//возвращаем время конца игры
-function getTimeEnd() {
-    var flagend = 1;
-    $('div .box').each(function (index, block) {
-        if (block.style.backgroundColor === 'rgb(255, 255, 255)') {
-            flagend = 0;
-            return flagend;
-        }
-    });
-    return flagend;
+//считаем итоговую сумму
+function sumLic(col, sum) {
+    let itog = col * sum;
+    return itog;
 }
